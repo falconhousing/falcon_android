668cf2cdb108c8e14c0ef0d39d45695de9ea52fd
Yogesh Gupta <yogesh@Yogeshs-MacBook-Pro.local>
2015-10-17 01:53:48 +0530

    more shit

---
+++ app/src/main/java/com/locon/withu/android/ui/ChannelFetcherTask.java
@@ -0,0 +1,41 @@
+package com.locon.withu.android.ui;
+
+import android.os.AsyncTask;
+
+import com.locon.withu.Constants;
+import com.locon.withu.utils.NetworkUtils;
+import com.locon.withu.utils.Utils;
+import com.squareup.okhttp.Response;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yogesh on 17/10/15.
+ */
+public class ChannelFetcherTask extends AsyncTask<Void, Void, ArrayList<Channel>> {
+
+    private ChannelsFetcherListener listener;
+
+    public ChannelFetcherTask(ChannelsFetcherListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    protected ArrayList<Channel> doInBackground(Void... params) {
+        String url = Constants.GET_CHANNELS_URL;
+        Response response = NetworkUtils.doGetCall(url);
+        ArrayList<Channel> Channels = Utils.parse(response, ChannelWrapper.class).stories;
+        return Channels;
+    }
+
+    @Override
+    protected void onPostExecute(ArrayList<Channel> Channels) {
+        if (Channels != null)
+            listener.onChannelsFetched(Channels);
+    }
+
+    public interface ChannelsFetcherListener {
+
+        void onChannelsFetched(ArrayList<Channel> Channels);
+    }
+}
---
+++ app/src/main/java/com/locon/withu/android/ui/ChannelWrapper.java
@@ -0,0 +1,12 @@
+package com.locon.withu.android.ui;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yogesh on 17/10/15.
+ */
+public class ChannelWrapper {
+
+    public ArrayList<Channel> stories;
+
+}
