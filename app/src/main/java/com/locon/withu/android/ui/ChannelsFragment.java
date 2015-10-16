9a4288e994dd008df71d35cb054b0686ed6063f3
Yogesh Gupta <yogesh@Yogeshs-MacBook-Pro.local>
2015-10-16 19:26:45 +0530

    butter knife, skeleton fragment added

---
+++ app/src/main/java/com/locon/withu/android/ui/ChannelsFragment.java
@@ -0,0 +1,41 @@
+package com.locon.withu.android.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.locon.withu.R;
+
+import butterknife.ButterKnife;
+
+
+public class ChannelsFragment extends Fragment {
+
+    public static ChannelsFragment newInstance() {
+        ChannelsFragment fragment = new ChannelsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_channels, container, false);
+        ButterKnife.inject(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews();
+    }
+
+    private void initViews() {
+    }
+}
---
+++ app/src/main/java/com/locon/withu/android/ui/LocationsFragment.java
@@ -0,0 +1,41 @@
+package com.locon.withu.android.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.locon.withu.R;
+
+import butterknife.ButterKnife;
+
+
+public class LocationsFragment extends Fragment {
+
+    public static LocationsFragment newInstance() {
+        LocationsFragment fragment = new LocationsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_locations, container, false);
+        ButterKnife.inject(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews();
+    }
+
+    private void initViews() {
+    }
+}
